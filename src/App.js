5da64793db1781daeaadc57b703a35c74895aa05
msi-ism <msism720@gmail.com>
2022-11-17 11:43:16 -0500

    msi card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import MSCard from  './components/msi-component'
+
 
 function App() {
   return (
     <div className="App">
+      <MSCard />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/components/msi-component/index.js
+++ src/components/msi-component/index.js
@@ -1,12 +1,22 @@
-
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import pp from './msi.jpg'
 
 
 
 const MSICard = () => {
     return (
-        <div>
-            <h1>Michael S. Irby</h1>
-        </div>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={pp} />
+            <Card.Body>
+                <Card.Title>Michael S. Irby</Card.Title>
+                <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                </Card.Text>
+                <a href='https://msi-ism.github.io' target='_blank'>Portfolio</a>
+            </Card.Body>
+        </Card>
     )
 }
 
